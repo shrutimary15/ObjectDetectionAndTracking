31f2b6b9e78c2c5ecdfd0eaf63ee4a18c71c5a33
shrutimary15 <shanshru15@gmail.com>
2023-09-02 13:05:36 +0530

    Updating

---
+++ constants.py
@@ -0,0 +1,6 @@
+RESIZE = (1024, 720)
+MODEL = 'yolov8n.pt'
+SOURCE = 'Artifacts\cut.mp4'
+AREA1 = [(88,380),(90,261),(287,320),(283,442)]   #red
+AREA2 = [(80,208),(302,281),(316,272),(95,206)]   #green
+LOG_DIR = "logs"
